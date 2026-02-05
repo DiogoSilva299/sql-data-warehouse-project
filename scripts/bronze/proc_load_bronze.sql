cf41ad11653a22bc33d9da72aa474b950e64d230
Diogo Silva <160676631+DiogoSilva299@users.noreply.github.com>
2026-02-05 15:23:47 +0000

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -28,7 +28,7 @@ set @batch_start_time = getdate();
 set @start_time = getdate();
 truncate table [bronze].[crm_cust_info]
 bulk insert [bronze].[crm_cust_info]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
+from 'C:\***\source_crm\cust_info.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
@@ -42,7 +42,7 @@ print('>> Data inserted in bronze.crm_cust_info');
 set @start_time = getdate();
 truncate table [bronze].[crm_prd_info]
 bulk insert [bronze].[crm_prd_info]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
+from 'C:\***\source_crm\prd_info.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
@@ -56,7 +56,7 @@ print('>> Data inserted in bronze.crm_prd_info');
 set @start_time = getdate();
 truncate table [bronze].[crm_sales_details]
 bulk insert [bronze].[crm_sales_details]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
+from 'C:\***\source_crm\sales_details.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
@@ -74,7 +74,7 @@ print('==================================================')
 set @start_time = getdate();
 truncate table [bronze].[erp_cust_az12]
 bulk insert [bronze].[erp_cust_az12]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_erp\cust_az12.csv'
+from 'C:\***\source_erp\cust_az12.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
@@ -88,7 +88,7 @@ print('>> Data inserted in bronze.erp_cust_az12');
 set @start_time = getdate();
 truncate table [bronze].[erp_loc_a101]
 bulk insert [bronze].[erp_loc_a101]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_erp\loc_a101.csv'
+from 'C:\***\source_erp\loc_a101.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
@@ -102,7 +102,7 @@ print('>> Data inserted in bronze.erp_loc_a101');
 set @start_time = getdate();
 truncate table [bronze].[erp_px_cat_g1v2]
 bulk insert [bronze].[erp_px_cat_g1v2]
-from 'C:\Users\diogo\Desktop\Data Engineering\SQL\sql-data-warehouse-project-main\datasets\source_erp\px_cat_g1v2.csv'
+from 'C:\***\source_erp\px_cat_g1v2.csv'
 with (
 	firstrow = 2,
 	fieldterminator = ',',
